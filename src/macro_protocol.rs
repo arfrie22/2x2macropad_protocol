6f7ed70855d8a0e156892a14964875ed45bd87e9
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2022-12-28 00:23:20 -0500

    added section annotation for macros

--- src/macro_protocol.rs
+++ src/macro_protocol.rs
@@ -18,10 +18,32 @@ use packed_struct::prelude::PrimitiveEnum;
 pub enum MacroCommand {
     #[num_enum(default)]
     CommandTerminator = 0x00,
-    CommandDelay = 0x01,
-    CommandPressKey = 0x02,
-    CommandReleaseKey = 0x03,
-    CommandConsumer = 0x04,
-    CommandSetLed = 0x05,
-    CommandClearLed = 0x06,
+    CommandSectionAnnotation = 0x01,
+    CommandDelay = 0x02,
+    CommandPressKey = 0x03,
+    CommandReleaseKey = 0x04,
+    CommandConsumer = 0x05,
+    CommandSetLed = 0x06,
+    CommandClearLed = 0x07,
+}
+
+#[repr(u8)]
+#[derive(
+    Debug,
+    Copy,
+    Clone,
+    Eq,
+    PartialEq,
+    Ord,
+    PartialOrd,
+    PrimitiveEnum,
+    Hash,
+    IntoPrimitive,
+    FromPrimitive,
+)]
+pub enum MacroSectionAnnotation {
+    #[num_enum(default)]
+    None = 0x00,
+    String = 0x01,
+    Chord = 0x02,
 }
