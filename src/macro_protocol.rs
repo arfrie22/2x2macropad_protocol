8e88479a2c8acc7bc4ec35ac52042c3fbd574145
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2023-01-04 21:51:25 -0500

    major change to macro system

--- src/macro_protocol.rs
+++ src/macro_protocol.rs
@@ -17,39 +17,15 @@ use packed_struct::prelude::PrimitiveEnum;
 )]
 pub enum MacroCommand {
     #[num_enum(default)]
-    CommandTerminator = 0x00,
-    CommandSectionAnnotation = 0x01,
-    CommandDelay = 0x02,
-    CommandPressKey = 0x03,
-    CommandReleaseKey = 0x04,
-    CommandConsumer = 0x05,
-    CommandReleaseConsumer = 0x06,
-    CommandSetLed = 0x07,
-    CommandClearLed = 0x08,
-}
-
-#[repr(u8)]
-#[derive(
-    Debug,
-    Copy,
-    Clone,
-    Eq,
-    PartialEq,
-    Ord,
-    PartialOrd,
-    PrimitiveEnum,
-    Hash,
-    IntoPrimitive,
-    FromPrimitive,
-)]
-pub enum MacroSectionAnnotation {
-    #[num_enum(default)]
-    None = 0x00,
-    KeyPress = 0x01,
-    ConsumerPress = 0x02,
-    String = 0x03,
-    Chord = 0x04,
-    LoopBegin = 0x05,
-    LoopIteration = 0x06,
-    LoopEnd = 0x07,
+    Empty = 0x00,
+    LoopBegin = 0x01,
+    LoopEnd = 0x02,
+    CommandSetLed = 0x03,
+    CommandClearLed = 0x04,
+    KeyDown = 0x05,
+    KeyUp = 0x06,
+    KeyPress = 0x07,
+    ConsumerPress = 0x08,
+    TypeString = 0x09,
+    Chord = 0x0A,
 }
